e7a3942187b1c865baa96be8f3896877e8309369
SimGyuYeon <55772767+SimGyuYeon@users.noreply.github.com>
2023-09-19 15:32:05 +0900

    로그인 페이지

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -1,28 +1,41 @@
 <template>
-  <div>
-    <h2>Login</h2>
-    <div>
-      <div>
-        <input
-          type="text"
-          id="account"
-          placeholder="Account"
-          v-model="user.account"
-        />
-      </div>
-      <div>
-        <input
-          type="password"
-          id="password"
-          placeholder="Password"
-          v-model="user.password"
-        />
-      </div>
-      <div>
-        <button @click="login">Login</button>
+  <div class="limiter">
+    <div class="container-login100">
+      <div class="wrap-login100">
+        <form class="login100-form validate-form">
+          <h1 :style="{fontFamily: 'proxima-nova-soft, sans-serif', fontWeight: '400', fontSize: '2.0rem', paddingBottom: '200px', width: '100%'}">
+            단기 시계열 예측 시스템
+          </h1>
+          <span class="login100-form-title p-b-34">
+            Account Login
+          </span>
+          <div class="wrap-input100 rs1-wrap-input100 validate-input m-b-20" data-validate="Type user name">
+            <input id="first-name" class="input100" type="text" name="username" placeholder="User name">
+            <span class="focus-input100"></span>
+          </div>
+          <div class="wrap-input100 rs2-wrap-input100 validate-input m-b-20" data-validate="Type password">
+            <input class="input100" type="password" name="pass" placeholder="Password">
+            <span class="focus-input100"></span>
+          </div>
+          <div class="container-login100-form-btn">
+            <button class="login100-form-btn" @click="login">
+              Log in
+            </button>
+          </div>
+          <div class="w-full text-center p-t-27 p-b-239">
+            <!-- <span class="txt1">
+              Forgot
+            </span>
+            <a href="#" class="txt2">
+              User name / password?
+            </a> -->
+          </div>
+        </form>
+        <div class="login100-more" :style="{backgroundImage: `url(${require('@/assets/img/kdn.png')})`, backgroundSize: '80%'}"></div>
       </div>
     </div>
   </div>
+  
 </template>
 
 <script>
@@ -33,6 +46,9 @@ export default {
         account: "",
         password: "",
       },
+      cssProps: {
+          backgroundImage: `url(${require('@/assets/img/kdn.png')})`
+        }
     };
   },
   methods: {
